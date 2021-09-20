e3ebbb89ea0f9f841c4ee7b7e465a39439ecad51
brightsummit.co <naseemoyebola@gmail.com>
2021-09-21 00:21:15 +0100

    Model/Endpoint

---
+++ SwiftUiMock/SwiftUiMock/Endpoint/NewsEndpoint.swift
@@ -0,0 +1,8 @@
+//
+//  NewsEndpoint.swift
+//  SwiftUiMock
+//
+//  Created by naseem on 21/09/2021.
+//
+
+import Foundation
---
+++ SwiftUiMock/SwiftUiMock/Model/NewsResponse.swift
@@ -0,0 +1,29 @@
+//
+//  NewsResponse.swift
+//  SwiftUiMock
+//
+//  Created by naseem on 21/09/2021.
+//
+
+import Foundation
+// MARK: - Welcome
+struct NewsResponse: Codable {
+    let articles: [Article]
+}
+
+// MARK: - Article
+struct Article: Codable, Identifiable {
+    let id = UUID()
+    let author: String?
+    let url: String
+    let source, title: String
+    let articleDescription: String?
+    let image: String?
+    let date: Date
+
+    enum CodingKeys: String, CodingKey {
+        case author, url, source, title
+        case articleDescription = "description"
+        case image, date
+    }
+}
