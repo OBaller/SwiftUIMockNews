7a0101d91cd468ff86561e03ce01020ef51d3686
brightsummit.co <naseemoyebola@gmail.com>
2021-09-21 00:14:04 +0100

    home view

---
+++ SwiftUiMock/SwiftUiMock/SwiftUiMockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiMockApp.swift
+//  SwiftUiMock
+//
+//  Created by IBE on 20/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiMockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
